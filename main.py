fe41d49ca7740d84539ddb015c99bc0f370b73f1
Aurélien Alet <aurelien.alet@cdbdx.biz>
2024-08-13 10:43:53 +0200

    feat: ajout precommit + nouvel endpoint

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/another-endpoint")
+async def root():
+    return {"message": "Another endpoint"}
